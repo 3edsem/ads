8d95af567640db5556aa8458193d2fca5b2114b3
3edsem <160483215+3edsem@users.noreply.github.com>
2024-03-19 21:43:40 +0530

    Update prg2.c

--- prg2.c
+++ prg2.c
@@ -1,41 +1,52 @@
 #include <stdio.h>
+#include <string.h>
 
-char str[100], pat[20], rep[20], newstr[100];
-int i = 0, j = 0, k, n = 0, exist = 0, l = 0;
+char str[50], pat[20], rep[20], res[50];
+int c = 0, m = 0, i = 0, j = 0, k, flag = 0;
 
-void matchAndReplace();
-
-int main() {
-    printf("\nEnter the string:");
-    fgets(str, sizeof(str), stdin);
-    printf("\nEnter the pattern:");
-    fgets(pat, sizeof(pat), stdin);
-    printf("\nEnter the replacing pattern:");
-    fgets(rep, sizeof(rep), stdin);
-    matchAndReplace();
-    return 0;
+void stringmatch() {
+    while (str[c] != '\0') {
+        if (str[m] == pat[i]) {
+            i++;
+            m++;
+            if (pat[i] == '\0') {
+                flag = 1;
+                for (k = 0; rep[k] != '\0'; k++, j++) {
+                    res[j] = rep[k];
+                }
+                i = 0;
+                c = m;
+            }
+        } else {
+            res[j] = str[c];
+            j++;
+            c++;
+            m = c;
+            i = 0;
+        }
+    }
+    res[j] = '\0';
 }
 
-void matchAndReplace() {
-    while (str[i] != '\0') {
-        j = 0, k = i;
-        while (str[k] == pat[j] && pat[j] != '\0') {
-            k++;
-            j++;
-        }
-        if (pat[j] == '\0') {
-            exist = 1;
-            for (l = 0; rep[l] != '\0'; l++)
-                newstr[n++] = rep[l];
-            i = k;
-        } else
-            newstr[n++] = str[i++];
-    }
-    if (!exist)
-        printf("\nPattern does not exist");
-    else {
-        newstr[n] = '\0';
-        printf("\nThe new string is:");
-        puts(newstr);
-    }
+int main() {
+    printf("Enter the main string:");
+    fgets(str, sizeof(str), stdin);
+    printf("\nEnter the pat string:");
+    fgets(pat, sizeof(pat), stdin);
+    printf("\nEnter the replace string:");
+    fgets(rep, sizeof(rep), stdin);
+
+    // Removing newline characters from inputs
+    str[strcspn(str, "\n")] = '\0';
+    pat[strcspn(pat, "\n")] = '\0';
+    rep[strcspn(rep, "\n")] = '\0';
+
+    printf("\nThe string before pattern match is:\n %s\n", str);
+    stringmatch();
+    if (flag == 1)
+        printf("\nThe string after pattern match and replace is:\n %s\n", res);
+    else
+        printf("\nPattern string is not found\n");
+
+    return 0;
 }
